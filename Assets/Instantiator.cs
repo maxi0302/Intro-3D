0bb6c146de28aa174fb1a2a315641e4505c4eb39
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-05-13 12:04:52 -0300

    Act

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Spawner()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
